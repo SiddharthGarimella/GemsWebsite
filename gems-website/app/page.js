04765a2d9013ded42dec337fed26db7ccc135cfd
Alex Cismaru <acismaru420@student.fuhsd.org>
2023-11-14 22:16:48 -0800

    website created

---
+++ gems-website/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+//THIS IS THE HOME PAGE OF THE WEBSITE
+export default function Home() {
+  return (
+    //Add HTML Components here
+    <main>
+
+
+    </main>
+  )
+}
