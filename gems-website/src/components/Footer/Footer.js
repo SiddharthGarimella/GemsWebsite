feef8f43ff3a8b4ca8c9e06e254f37d99c88c038
cqtqlyst <aayush.gaywala@gmail.com>
2023-11-29 13:29:54 -0800

    akshay & ada code

--- gems-website/src/components/Footer/Footer.js
+++ gems-website/src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
 function Footer() {
-  return <div class="Footer">Footer</div>;
+  return (
+    <div class="Footer">
+      Footer
+      <p></p>
+    </div>
+  );
 }
 
 export default Footer;
--- gems-website/src/pages/AboutUs/AboutUs.js
+++ gems-website/src/pages/AboutUs/AboutUs.js
@@ -1,5 +1,34 @@
 function AboutUs() {
-  return <div class="AboutUs">About Us</div>;
+  return (
+    <div class="AboutUs">
+      About Us
+      <p>
+        {" "}
+        After having worked as a preschool teacher for 5 years at Challenger
+        School, and for a few years at Stratford School, my love for kiddos and
+        my passion for teaching motivated me to start my own child care. I want
+        to create an environment that is nurturing, where kiddos are cared for
+        with respect, treated with dignity keeping in mind their own unique
+        characteristics.
+      </p>
+      <p>
+        {" "}
+        In our competitive society it is important for students to not only
+        receive a solid education but to work with someone who is aware and
+        sensitive to their individual needs and capabilities. I believe that
+        children are like young plants, different in their own ways but if
+        nurtured well, they will all blossom to their full potential.
+      </p>
+      <p>
+        {" "}
+        Here at Gems Academy I provide a very fun filled; activity packed;
+        learning environment which will not only instill in children a love for
+        learning but also enhance their confidence and self-esteem as they
+        develop their speaking, reading, writing, fine/gross motor and social
+        skills.
+      </p>
+    </div>
+  );
 }
 
 export default AboutUs;
